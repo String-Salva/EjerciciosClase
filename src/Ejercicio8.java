768d6ba648be85ff9470438ecf3a5b2b2d77a21e
Salva <salvador.s12901@cesurformacion.com>
2026-01-08 13:24:26 +0100

    Ejercicio 7 actualizado

---
+++ src/Ejercicio8.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Ejercicio8 {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce");
+    }
+}
