1908a7f9aa0179926238d1e857720f3986ac3a72
Salva <salvador.s12901@cesurformacion.com>
2026-02-12 14:26:56 +0100

    ej 4

--- src/unidad6/ejercicio4/Inventario.java
+++ src/unidad6/ejercicio4/Inventario.java
@@ -1,4 +1,12 @@
 package unidad6.ejercicio4;
 
 public class Inventario {
+
+    int total = Producto.totalProductos;
+    Producto[] productos = new Producto[total];
+
+
+    public void agregarProducto(Producto producto) {
+
+    }
 }
