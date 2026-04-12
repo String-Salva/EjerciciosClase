3477e9b333082b14c5a63aa45ad33421ed0649f6
Salva <string.salva@gmail.com>
2026-04-12 22:08:37 +0200

    Ejercicio 2 terminado

--- src/swing/ej2/Calificaciones.java
+++ src/swing/ej2/Calificaciones.java
@@ -2,6 +2,7 @@ package swing.ej2;
 
 import swing.ej2.excepciones.CalificationsLimitException;
 import swing.ej2.excepciones.NotaInvalidaException;
+import swing.ej2.Herramientas;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -9,6 +10,7 @@ import java.awt.event.ActionListener;
 import java.io.*;
 
 // Como en el ejercicio anterior, esta clase me la ha creado sola el diseñador de Swing y ha declarado como atributos los componentes que se van a utilizar
+
 public class Calificaciones extends JFrame {
     private JPanel mainPanel;
     private JPanel panelBotones;
@@ -17,10 +19,14 @@ public class Calificaciones extends JFrame {
     private JButton cargarNotasButton;
     private JTextField campoNombre;
     private JTextField campoNota;
-    private JTextField campoListadoNotas;
+    private JTextArea campoListadoNotas;
     private JLabel listadoLabel;
     private JLabel calificacionLabel;
     private JLabel nombreLabel;
+
+
+
+
 
     // Voy a ir declarando las variables que considero importantes para la lógica de este ejercicio
 
@@ -28,60 +34,11 @@ public class Calificaciones extends JFrame {
     double[] notas = new double[30];
     int contador = 0;
 
-    //Voy a crear un método de validación para que no haya alumnos repetidos
-    public boolean yaTieneNota(String nombre) {
-        for (int i = 0; i < nombres.length; i++) {
-            if (nombre.equals(nombres[i])) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    // Voy a crear los métodos para añadir las calificaciones a los Arrays creados, obligandome posteriormente a gestionar error si se llena el almacenamiento
-
-    public void addNotas(String nombre, String nota) throws CalificationsLimitException, NotaInvalidaException {
-        if (contador == nombres.length) {
-            throw new CalificationsLimitException("Está el almacenamiento de calificaciones lleno");
-        } else if (Double.parseDouble(nota) < 0.0 || Double.parseDouble(nota) > 10.0) {
-            throw new NotaInvalidaException("La nota no puede ser negativa ni por encima del máximo (10)");
-        } else {
-            nombres[contador] = nombre;
-            notas[contador] = Double.parseDouble(nota); // Lo hago así porque el parámetro lo va a coger como cadena de texto
-            contador++;
-        }
-    }
-
-    // Método para actualizar listado
-
-    public void updateListadoNotas() {
-        String s = "";
-        for (int i = 0; i < nombres.length; i++) {
-            if (nombres[i] != null) {
-                s += "Nombre: " + nombres[i] + " Nota: " + notas[i] + "\n";
-            } else {
-                break;
-            }
-        }
-    }
 
     // Método para limpiar los inputs
     public void clearInputs() {
         campoNombre.setText("");
         campoNota.setText("");
-    }
-
-    // Método para confirmar si se sobreescribe la nota a un usuario que ya existe en memoria
-    // Según he visto, este método además de lanzar la pantalla, devuelve 0 si el usuario ha pulsado SÍ y devuelve 1 si el usuario ha pulsado NO
-    public void confirmarNotas(String alumno) {
-        int siOno = JOptionPane.showConfirmDialog(
-                null,
-                "Este nombre está guardado en memoria, estás seguro de que quieres sobreescribir su nota?", "Advertencia", JOptionPane.YES_NO_OPTION);
-        if (siOno == 0) {
-            for (int i = 0; i < campoNota.getText().length(); i++) {
-
-            }
-        }
     }
 
 
@@ -94,27 +51,49 @@ public class Calificaciones extends JFrame {
         setVisible(true);
         setLocationRelativeTo(null);
 
+
         // Este es el listener del botón agregar nota
 
         agregarNotaButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    if (yaTieneNota(campoNombre.getText())) {
-
+                    if (Herramientas.yaTieneNota(campoNombre.getText(), nombres)) {
+                        Herramientas.confirmarNotas(campoNombre.getText(), Double.parseDouble(campoNota.getText()), nombres, notas, campoListadoNotas);
+                        clearInputs();
                     }
-                    if ((!campoNombre.getText().isEmpty() || !campoNota.getText().isEmpty())) {
+                    if ((!campoNombre.getText().isEmpty() && !campoNota.getText().isEmpty())) {
 
-                        addNotas(campoNombre.getText(), campoNota.getText());
+                        Herramientas.addNotas(contador, campoNombre.getText(), campoNota.getText(), nombres, notas, campoListadoNotas);
+
+                        contador++; // No estoy orgullosos de esta chapuza, pero lo he visto necesario porque era más complicado hacer que se actualice el contador desde métodos estáticos
+                    } else {
+                        throw new NotaInvalidaException("Algún campo está vacío, reintroduce los valores");
                     }
+
+                    clearInputs();
                 } catch (
                         NumberFormatException error) { // Capturo esta Exception porque para el método, le entró el parámetro como texto y puede fallar en la conversión
+
                     // El método que voy a utilizar a continuación es para que salga una pantalla emergente avisando al usuario de que algo está mal
                     JOptionPane.showMessageDialog(null, "La nota ingresada no es un número");
                 } catch (CalificationsLimitException |
                          NotaInvalidaException error) { // Estas son las dos excepciones que he creado para validaciones
                     JOptionPane.showMessageDialog(null, error.getMessage());
                 }
+            }
+        });
+        guardarNotasButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Herramientas.guardarNotas(Herramientas.updateListadoNotas(nombres,notas));
+                JOptionPane.showMessageDialog(null, "Notas guardadas con exito");
+            }
+        });
+        cargarNotasButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Herramientas.cargarNotas(nombres,notas,campoListadoNotas);
             }
         });
     }
---
+++ src/swing/ej2/Herramientas.java
@@ -0,0 +1,132 @@
+package swing.ej2;
+
+import swing.ej2.excepciones.CalificationsLimitException;
+import swing.ej2.excepciones.NotaInvalidaException;
+
+import javax.swing.*;
+import java.io.*;
+
+public class Herramientas {
+    //Voy a crear un método de validación para que no haya alumnos repetidos
+    public static boolean yaTieneNota(String nombre, String[] nombres) {
+        for (int i = 0; i < nombres.length; i++) {
+            if (nombre.equals(nombres[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Voy a crear los métodos para añadir las calificaciones a los Arrays creados, obligandome posteriormente a gestionar error si se llena el almacenamiento
+
+    public static void addNotas(int contador, String nombre, String nota, String[] nombres, double[] notas, JTextArea a) throws CalificationsLimitException, NotaInvalidaException {
+        if (contador == nombres.length) {
+            throw new CalificationsLimitException("Está el almacenamiento de calificaciones lleno");
+        } else if (Double.parseDouble(nota) < 0.0 || Double.parseDouble(nota) > 10.0) {
+            throw new NotaInvalidaException("La nota no puede estar vacía, ser negativa ni ser por encima del máximo (10)");
+        } else {
+            nombres[contador] = nombre;
+            notas[contador] = Double.parseDouble(nota);
+            mostrarNotas(nombres, notas, a);
+        }
+    }
+
+
+    // Método para formatear las líneas del TextArea
+
+    public static String updateListadoNotas(String[] nombres, double[] notas) {
+        String s = "";
+        for (int i = 0; i < nombres.length; i++) {
+            if (nombres[i] != null) {
+                s += nombres[i] + "   ->   " + notas[i] + "\n";
+            } else {
+                break;
+            }
+        }
+        return s;
+    }
+
+
+    // Método para confirmar si se sobreescribe la nota a un usuario que ya existe en memoria
+    // Según he visto, este método además de lanzar la pantalla, devuelve 0 si el usuario ha pulsado SÍ y devuelve 1 si el usuario ha pulsado NO
+    public static void confirmarNotas(String alumno, double nota, String[] nombres, double[] notas, JTextArea textArea) {
+        int siOno = JOptionPane.showConfirmDialog(
+                null,
+                "Este nombre está guardado en memoria, estás seguro de que quieres sobreescribir su nota?", "Advertencia", JOptionPane.YES_NO_OPTION);
+        if (siOno == 0) {
+            for (int i = 0; i < nombres.length; i++) {
+                if (nombres[i] != null) {
+                    if (nombres[i].equals(alumno)) {
+                        notas[i] = nota;
+                    } else {
+                        notas[i] = notas[i];
+                    }
+                } else {
+                    break;
+                }
+            }
+
+
+        } else {
+
+            mostrarNotas(nombres, notas, textArea);
+        }
+    }
+
+
+    // Método para mostrar listado de notas conforme se agrega
+
+    public static void mostrarNotas(String[] nombres, double[] notas, JTextArea textArea) {
+        textArea.setText(updateListadoNotas(nombres, notas));
+    }
+
+
+    // Ahora voy a hacer los métodos para guardar notas
+
+    public static void guardarNotas(String notas) {
+        File file = new File("directorioNotas/notas.txt");
+        FileWriter fw = null;
+        BufferedWriter bw = null;
+        try {
+            fw = new FileWriter(file);
+            bw = new BufferedWriter(fw);
+            bw.write(notas);
+
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Error al guardar");
+        }
+    }
+
+    // Ahora un método para cargar las notas del archivo de texto creado previamente
+
+    public static void cargarNotas(String[] nombres, double[] notas, JTextArea textArea) {
+        File archivo = new File("directorioNotas/notas.txt");
+        FileReader fr = null;
+        BufferedReader br = null;
+        try {
+            fr = new FileReader(archivo);
+            br = new BufferedReader(fr);
+            String linea;
+            int i = 0;
+            while ((linea = br.readLine()) != null) {
+                String[] separador = linea.split("   ->   "); // Iba a hacerlo con separadores estilo | y me daba problemas
+
+                nombres[i] = separador[0].trim();
+                notas[i] = Double.parseDouble(separador[1].trim());
+
+                i++;
+
+            }
+            br.close();
+            fr.close();
+            mostrarNotas(nombres, notas, textArea);
+
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Archivo no encontrado");
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Error al cargar");
+        }
+    }
+}
