465051df02064f043e310766327781ce1a74d20d
Salva <string.salva@gmail.com>
2026-05-02 15:59:44 +0200

    ejercicio de programación funcional

---
+++ src/programacionfuncional/Coche.java
@@ -0,0 +1,40 @@
+package programacionfuncional;
+
+public class Coche {
+  private String marca;
+  private String modelo;
+  private double precio;
+
+    public Coche(String marca, String modelo, double precio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = precio;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+
+  
+}
---
+++ src/programacionfuncional/Practica.java
@@ -0,0 +1,11 @@
+package programacionfuncional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Practica {
+  List<Coche> coches = new ArrayList<>();
+
+  coches.add(new Coche("Volkswagen","Amarok",25000.0));
+  
+}
